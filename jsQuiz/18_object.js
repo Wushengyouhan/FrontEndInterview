045b27d03925e8fc217e122dc9ae807a909c838c
Wushengyouhan <840835067@qq.com>
2025-09-04 16:41:09 +0800

    feat: jsQuiz19

--- jsQuiz/18_object.js
+++ jsQuiz/18_object.js
@@ -1,11 +1,13 @@
-function checkAge(data) {
-  if (data === { age: 18 }) {
-    console.log('You are an adult!')
-  } else if (data == { age: 18 }) {
-    console.log('You are still an adult.')
-  } else {
-    console.log(`Hmm.. You don't have an age I guess`)
-  }
-}
+// 警告暂时注释掉
 
-checkAge({ age: 18 })
+// function checkAge(data) {
+//   if (data === { age: 18 }) {
+//     console.log('You are an adult!')
+//   } else if (data == { age: 18 }) {
+//     console.log('You are still an adult.')
+//   } else {
+//     console.log(`Hmm.. You don't have an age I guess`)
+//   }
+// }
+
+// checkAge({ age: 18 })
